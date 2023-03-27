371b7437ae928adcf040d8c1d5166a1ee16d814d
GeneralPlayfix <81032503+GeneralPlayfix@users.noreply.github.com>
2023-03-27 20:10:07 +0200

    feat(unit test) unit test with mocha

---
+++ test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const evaluate = require('./utils');
+
+describe('evaluate.customEval', function() {
+  it('should evaluate simple arithmetic expressions', function() {
+    assert.strictEqual(evaluate.customEval('2+3'), 5);
+    assert.strictEqual(evaluate.customEval('2-3'), -1);
+    assert.strictEqual(evaluate.customEval('2*3'), 6);
+    assert.strictEqual(evaluate.customEval('6/3'), 2);
+    assert.strictEqual(evaluate.customEval('7%4'), 3);
+  });
+
+  it('should handle parentheses', function() {
+    assert.strictEqual(evaluate.customEval('(2+3)*4'), 20);
+    assert.strictEqual(evaluate.customEval('5/2'), 2.5);
+  });
+
+  it('should handle negative numbers', function() {
+    assert.strictEqual(evaluate.customEval('-2+3'), 1);
+    assert.strictEqual(evaluate.customEval('2*-3'), -6);
+    assert.strictEqual(evaluate.customEval('(-2+3)*(-4)'), -4);
+  });
+
+  it('should throw errors for invalid syntax', function() {
+    assert.throws(() => evaluate.customEval('2+3)'), /Mismatched parentheses/);
+    assert.throws(() => evaluate.customEval('2/0'), /Division by zero/);
+    assert.throws(() => { evaluate.customEval("(1+2"); }, Error, "Mismatched parentheses");
+    assert.throws(() => { evaluate.customEval("1 / 0"); }, Error, "Division by zero");
+    assert.throws(() => { evaluate.customEval("1 % 0"); }, Error, "Modulo by zero");
+    assert.throws(() => evaluate.customEval('(2+3'), /Mismatched parentheses/);
+    assert.throws(() => evaluate.customEval('2+3^2'), /Unknown operator/);
+  });
+});
